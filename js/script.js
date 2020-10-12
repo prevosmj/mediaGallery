e386994595b5c1a0f17c3adfd32a842cc9926ca5
LavusVincere <mikeprevost999@gmail.com>
2020-10-12 12:59:08 -0400

    Added "Splide" to replace basic slideshow function, fixed model to open.

--- js/script.js
+++ js/script.js
@@ -7,34 +7,17 @@ function toggleNav() {
     }
   } 
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-  } 
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#slider', {
+      type: 'loop',
+      perPage: 3,
+      focus: 'center',
+      padding: {
+          right: '5rem',
+          left: '5rem',
+      },
+  } ).mount();
+} );
 
 $(".button").on("click", function() { 
   var modal = $(this).data("modal"); 
